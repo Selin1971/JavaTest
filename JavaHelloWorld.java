c20f99c4f487121ea0e8474c163033dd4e926225
Selin1971 <selin-a-e@yandex.ru>
2020-04-10 16:15:23 +0400

    Edit 1

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,5 +1,9 @@
 public class JavaHelloWorld {
 	public static void main (String[] arg){
 		System.out.println("Привет Мир !!!");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+        int rez=first+second;
+		System.out.println("REZ="+rez);
 	}	
 }
